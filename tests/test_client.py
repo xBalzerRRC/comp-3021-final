8c39442018ad9ec609b3e9c8ba1b61d1fb930bc7
Xavier Balzer <xbalzer@rrc.com>
2025-01-26 12:37:19 -0600

    Added import statements, initial framework of the TestClient class, and slight changes to existing documentation.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,7 +1,25 @@
+"""This module defines the TestClient class.
+
+Example:
+    $ python -m unittest tests/test_client.py
 """
-Description: Unit tests for the Client class.
-Author: ACE Faculty
-Usage: To execute all tests in the terminal execute 
-the following command:
-    python -m unittest tests/test_client.py
-"""
+
+__author__ = "Xavier Balzer"
+__version__ = "1.0.0"
+
+import unittest
+from client.client import Client
+
+class TestClient(unittest.TestCase):
+    """Tests for the Client class."""
+
+    def SetUp(self):
+        client_number = 1234
+        first_name = "Xavier"
+        last_name = "Balzer"
+        email_address = "xbalzer@rrc.ca"
+
+        self.client = Client(client_number, first_name, 
+                             last_name, email_address)
+        
+        
