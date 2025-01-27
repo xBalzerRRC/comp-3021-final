3c7aca920d6f45bdbfd8daa7c1a27989593439de
Xavier Balzer <xbalzer@rrc.com>
2025-01-26 18:41:37 -0600

    Added basic documentation, TestBankAccount class, and setUp method.

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,7 +1,22 @@
+"""This module defines the TestBankAccount class.
+
+Example:
+    $ python -m unittest tests/test_bank_account.py
 """
-Description: Unit tests for the BankAccount class.
-Author: ACE Faculty
-Usage: To execute all tests in the terminal execute 
-the following command:
-    python -m unittest tests/test_bank_account.py
-"""
+
+__author__ = "Xavier Balzer"
+__version__ = "1.0.0"
+
+import unittest
+from bank_account.bank_account import BankAccount
+
+class TestBankAccount(unittest.TestCase):
+    """Tests for the BankAccount class."""
+
+    def setUp(self):
+        account_number = 1234
+        client_number = 1
+        balance = 1000.0
+
+        self.bankaccount = BankAccount(account_number, client_number, balance)
+        
