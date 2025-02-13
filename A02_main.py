ef482c39a4ef1d6bc70819e4f3816abc76db72dc
Xavier Balzer <xbalzer@rrc.com>
2025-02-12 21:26:45 -0600

    Added initial framework for BankAccount class expansion with subclasses and corresponding test files.

---
+++ A02_main.py
@@ -0,0 +1,77 @@
+"""
+Description: A client program written to verify correctness of 
+the BankAccount sub classes.
+"""
+__author__ = "ACE Faculty"
+__version__ = "1.0.0"
+__credits__ = ""
+
+# 1.  Import all BankAccount types using the bank_account package
+#     Import date from datetime
+
+
+# 2. Create an instance of a ChequingAccount with values of your 
+# choice including a balance which is below the overdraft limit.
+
+
+# 3. Print the ChequingAccount created in step 2.
+# 3b. Print the service charges amount if calculated based on the 
+# current state of the ChequingAccount created in step 2.
+
+
+# 4a. Use ChequingAccount instance created in step 2 to deposit 
+# enough money into the chequing account to avoid overdraft fees.
+# 4b. Print the ChequingAccount
+# 4c. Print the service charges amount if calculated based on the 
+# current state of the ChequingAccount created in step 2.
+
+
+print("===================================================")
+# 5. Create an instance of a SavingsAccount with values of your 
+# choice including a balance which is above the minimum balance.
+
+
+# 6. Print the SavingsAccount created in step 5.
+# 6b. Print the service charges amount if calculated based on the 
+# current state of the SavingsAccount created in step 5.
+
+
+# 7a. Use this SavingsAccount instance created in step 5 to withdraw 
+# enough money from the savings account to cause the balance to fall 
+# below the minimum balance.
+# 7b. Print the SavingsAccount.
+# 7c. Print the service charges amount if calculated based on the 
+# current state of the SavingsAccount created in step 5.
+
+
+
+print("===================================================")
+# 8. Create an instance of an InvestmentAccount with values of your 
+# choice including a date created within the last 10 years.
+
+
+# 9a. Print the InvestmentAccount created in step 8.
+# 9b. Print the service charges amount if calculated based on the 
+# current state of the InvestmentAccount created in step 8.
+
+
+# 10. Create an instance of an InvestmentAccount with values of your 
+# choice including a date created prior to 10 years ago.
+
+
+# 11a. Print the InvestmentAccount created in step 10.
+# 11b. Print the service charges amount if calculated based on the 
+# current state of the InvestmentAccount created in step 10.
+
+
+print("===================================================")
+
+# 12. Update the balance of each account created in steps 2, 5, 8 and 10 
+# by using the withdraw method of the superclass and withdrawing 
+# the service charges determined by each instance invoking the 
+# polymorphic get_service_charges method.
+
+
+
+# 13. Print each of the bank account objects created in steps 2, 5, 8 and 10.
+
--- README.md
+++ README.md
@@ -10,6 +10,8 @@ Xavier Balzer
 
 Assignment #1 - Classes: This assignment will produce some foundational classes to be used in an overall system. The classes will incorporate the outcomes associated with Module 01. Specifically, these classes will incorporate encapsulation through the use of private attributes and public accessors and mutators. Additionally, these classes will benefit from thorough unit testing, including unit test planning.
 
+Assignment #2 - Abstraction, Inheritance and Polymorphism: This assignment will extend the BankAccount class created in your previous assignment. The BankAccount class will be used as a superclass from which more specific subclasses will be derived. Each subclass will inherit attributes and methods from the superclass, and will incorporate functionality specific to the subclass. Polymorphism will be realized by having each subclass provide their own unique implementation to a superclass method. Unit testing in this assignment will be limited to verifying the expected polymorphic behaviour.
+
 ## Encapsulation
 
 By making use of accessors and mutators for the BankAccount class' attributes, we are able to read and assign values to private attributes without needing direct access to them. For instance, methods such as update_balance, withdraw, and deposit enable us to make changes to the balance class attribute without needing to modify the variable directly.
