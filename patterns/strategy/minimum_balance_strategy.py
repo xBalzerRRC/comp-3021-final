6431c01bff3ef3045f6430b9a3f9b131165b7a55
Xavier Balzer <xbalzer@rrc.com>
2025-03-16 14:36:51 -0500

    Implemented calculate_service_charges method with logic from SavingsAccount class' get_service_charges method.

--- patterns/strategy/minimum_balance_strategy.py
+++ patterns/strategy/minimum_balance_strategy.py
@@ -1,7 +1,7 @@
 """This module defines the MinimumBalanceStrategy subclass."""
 
 __author__ = "Xavier Balzer"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 from bank_account.bank_account import BankAccount
 from patterns.strategy.service_charge_strategy import ServiceChargeStrategy
@@ -20,3 +20,17 @@ class MinimumBalanceStrategy(ServiceChargeStrategy):
         self.SERVICE_CHARGE_PREMIUM = 2.0
         self.__minimum_balance = minimum_balance
 
+    def calculate_service_charges(self, account: BankAccount) -> float:
+        """Returns the calculated service charges a BankAccount will 
+            incur.
+
+        Returns:
+            float:  Calculated service charges.
+        """
+        
+        service_charges = self.BASE_SERVICE_CHARGE
+
+        if self.balance < self.__minimum_balance:
+            service_charges *= self.SERVICE_CHARGE_PREMIUM
+              
+        return service_charges
