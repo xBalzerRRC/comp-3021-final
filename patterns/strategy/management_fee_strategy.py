d80e46040bb9ec1a1bda2b660df648323e3c96f0
Xavier Balzer <xbalzer@rrc.com>
2025-03-16 14:34:33 -0500

    Added calculate_service_charges method with the same logic as the InvestmentAccount class' get_service_charges_method.

--- patterns/strategy/management_fee_strategy.py
+++ patterns/strategy/management_fee_strategy.py
@@ -1,7 +1,7 @@
 """This module defines the ManagementFeeStrategy subclass."""
 
 __author__ = "Xavier Balzer"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 from bank_account.bank_account import BankAccount
 from patterns.strategy.service_charge_strategy import ServiceChargeStrategy
@@ -25,3 +25,19 @@ class ManagementFeeStrategy(ServiceChargeStrategy):
 
         self.__date_created = date_created
         self.__management_fee = management_fee
+
+    def calculate_service_charges(self, account: BankAccount) -> float:
+        """Returns the calculated service charges a BankAccount will 
+            incur.
+
+        Returns:
+            float:  Calculated service charges.
+        """
+
+        service_charge = self.BASE_SERVICE_CHARGE
+
+        if self.TEN_YEARS_AGO < self._date_created:
+            service_charge += self._management_fee
+
+        return service_charge
+                
