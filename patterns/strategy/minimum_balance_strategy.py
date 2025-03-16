37667185e327f72cefdf9cdf65f22723d5fcfc9d
Xavier Balzer <xbalzer@rrc.com>
2025-03-16 14:35:33 -0500

    Created MinimumBalanceStrategy class and __init__ method.

---
+++ patterns/strategy/minimum_balance_strategy.py
@@ -0,0 +1,22 @@
+"""This module defines the MinimumBalanceStrategy subclass."""
+
+__author__ = "Xavier Balzer"
+__version__ = "1.0.0"
+
+from bank_account.bank_account import BankAccount
+from patterns.strategy.service_charge_strategy import ServiceChargeStrategy
+
+class MinimumBalanceStrategy(ServiceChargeStrategy):
+    """Represents a service charge strategy that 
+    """
+    def __init__(self, minimum_balance: float):
+        """Initializes a new instance of the MinimumBalanceStrategy subclass.
+        
+        Args:
+            minimum_balance(float): The minimum value a balance can be 
+                before further service charges are applied.
+        """
+
+        self.SERVICE_CHARGE_PREMIUM = 2.0
+        self.__minimum_balance = minimum_balance
+
